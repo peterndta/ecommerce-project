56bceb226e432df88ba90f82ee8c35def1ce85f8
Peter Nguyen <truonganh20062001@gmail.com>
2024-03-29 16:27:31 +0700

    Change ClientKey

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,6 +1,6 @@
 export default {
     oidc: {
-        clientId: '0oag05le4svpgump75d7',
+        clientId: '0oag3whohr7qBr3hY5d7',
         issuer: 'https://dev-85428653.okta.com/oauth2/default',
         redirectUri: 'https://ecommerce-project-sigma-inky.vercel.app/login/callback',
         scopes: ['openid', 'profile', 'email'],
